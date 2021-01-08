67a71e1b097acb50277d066526c613cfe68b0922
julio-cesar96 <julio_cbs96@hotmail.com>
2021-01-08 17:37:09 -0300

    update repo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// condição para para criação do slide em caso da função SimpleSlide() existir
+if (window.SimpleSlide) {
+/* SLIDE DE DEPOIMENTOS */
+    new SimpleSlide({
+        slide: "quote",
+        time: 4000
+    });
+/* SLIDE DE DEPOIMENTOS */
+
+/* SLIDE DE BIKES DO PORTIFOLIO */
+    new SimpleSlide({
+        slide: "portifolio",
+        time: 4000, 
+        nav: true
+    });
+/* SLIDE DE BIKES DO PORTIFOLIO */
+}
